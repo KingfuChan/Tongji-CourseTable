86e8aeace596f2a3d3f543c1b7b2dc657eaef78e
KingfuChan <kingfu_chan2000@tongji.edu.cn>
2019-09-09 00:14:42 +0800

    更新request方法

--- post2login.py
+++ post2login.py
@@ -1,31 +1,73 @@
-import urllib
-from http import cookiejar
+import re
+import requests
 
-idsurl = "https://ids.tongji.edu.cn:8443/nidp/saml2/sso?id=3423&sid=0&option=credential&sid=0"
-loginurl = "https://ids.tongji.edu.cn:8443/nidp/saml2/sso?sid=0&sid=0"
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
+def input_info():
+    userid = input("Enter ID:")
+    userpw = input("Enter Password:")
+    return userid, userpw
 
-userid = ''  # 待填
-userpw = ''
-params = {
-    'option': 'credential',
-    'Ecom_User_ID': userid,
-    'Ecom_Password': userpw,
-    'Txtidcode': '0000'}
-data = bytes(urllib.parse.urlencode(params), encoding='utf-8')
 
-cj = cookiejar.LWPCookieJar()
-cookie_support = urllib.request.HTTPCookieProcessor(cj)
-opener = urllib.request.build_opener(
-    cookie_support, urllib.request.HTTPHandler)
-urllib.request.install_opener(opener)
-loginopen = urllib.request.urlopen(loginurl, data=data)
+def saml_login(session):
+    """
+    step:
+    1. GET init_url    -> forward to sso_url
+    2. GET sso_url     -> get a form and get sid_url
+    3. POST sid_url    -> get login_url
+    4. POST login_url  -> submit login data
+    5. GET sid_url     -> get aseert_url
+    5. POST assert_url -> 302 and then OK!
+    """
+    # 1
+    base_url = 'https://ids.tongji.edu.cn:8443'
+    init_url = 'http://4m3.tongji.edu.cn/eams/samlCheck'
+    res = session.get(init_url)
 
-with open('request.html', 'w', encoding='utf-8') as f:
-    f.write(loginopen.read().decode('utf-8'))
+    # 2
+    sso_url = re.findall(r'url=(.*)"><', str(res.content))[0]
+    res = session.get(sso_url)
 
-with open('cookie.txt', 'w', encoding='utf-8') as c:
-    for j in cj:
-        c.write(f"{j.name} = {j.value}\n")
+    # 3
+    sid_url = base_url + re.findall(r'action="(.*)"><', str(res.content))[0]
+    res = session.post(sid_url)
+
+    # 4
+    login_url = sid_url
+    username, password = input_info()
+    data = {
+        'Ecom_Password': password,
+        'Ecom_User_ID': username,
+        'option': 'credential',
+        'submit': '登录'
+    }
+    res = session.post(login_url, data)
+
+    # 5
+    sid_url = re.findall(r"top.location.href=\\'(.*)\\';", str(res.content))[0]
+    res = session.get(sid_url)
+
+    # 6
+    assert_url = "http://4m3.tongji.edu.cn/eams/saml/SAMLAssertionConsumer"
+    data = {
+        'SAMLResponse': re.findall(r'value="(.*)"/>', str(res.content))[0]
+    }
+    res = session.post(assert_url, data)
+
+
+def get_coursetable(session):
+    url = "http://4m3.tongji.edu.cn/eams/courseTableForStd!courseTable.action"
+    data = {
+        'ignoreHead': '1',
+        'setting.kind': 'std',
+        'startWeek': '1',
+        'semester.id': '108',
+        'ids': '5836063678'  # 推测与用户挂钩，每个人对应不同的id
+    }
+    res = session.post(url, data)
+    open('res_temp.html', 'w').write(str(res.content))
+
+
+if __name__ == "__main__":
+    s = requests.Session()
+    saml_login(s)
+    get_coursetable(s)
