a24602ba416e12ab332b499123f94d492331a9e0
KingfuChan <kingfu_chan2000@tongji.edu.cn>
2019-09-09 14:00:16 +0800

    实现post登录并获取课表

--- post2login.py
+++ post2login.py
@@ -48,14 +48,28 @@ def saml_login(session):
 
     # 6
     assert_url = "http://4m3.tongji.edu.cn/eams/saml/SAMLAssertionConsumer"
+    # 使用str只能找到一个匹配项
+    values = re.findall(r'value="(.*)"/>', res.content.decode('utf-8'))
     data = {
-        'SAMLResponse': re.findall(r'value="(.*)"/>', str(res.content))[0]
+        'SAMLResponse': values[0],
+        'RelayState': values[1],
     }
     res = session.post(assert_url, data)
 
 
 def get_coursetable(session):
+    print(session.cookies)
     url = "http://4m3.tongji.edu.cn/eams/courseTableForStd!courseTable.action"
+    headers = {
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Origin': 'http://4m3.tongji.edu.cn',
+        'Referer': 'http://4m3.tongji.edu.cn/eams/courseTableForStd!index.action',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    session.headers.update(headers)
     data = {
         'ignoreHead': '1',
         'setting.kind': 'std',
@@ -64,7 +78,7 @@ def get_coursetable(session):
         'ids': '5836063678'  # 推测与用户挂钩，每个人对应不同的id
     }
     res = session.post(url, data)
-    open('res_temp.html', 'w').write(str(res.content))
+    open("res_tmp.txt", 'w').write(res.content.decode('utf-8'))
 
 
 if __name__ == "__main__":
